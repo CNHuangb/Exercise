26d1c271ee4a34dfc5be7410312f837eb504d2d5
cnhuangb <huangb.mmsh@outlook.com>
2025-05-26 15:04:09 +0800

    Test Exercise
    Test

--- main.py
+++ main.py
@@ -3,38 +3,97 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-
-    print("hello word")
-
-    print("hello word")
+# import streamlit as st
 
 
 
-    print("hello word")
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
 
 
 
-    print("hello word")
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello, FastAPI!"}
 
 
 
 
-    print("hello word")
-
-    print("hello word")
-
-    print("I love you")
 
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+
+#     print("hello word")
+
+#     print("hello word")
+
+
+
+#     print("hello word")
+
+
+
+#     print("hello word")
+
+
+
+
+#     print("hello word")
+
+#     print("hello word")
+
+#     print("I love you")
+
+#     st.write("测试")
+
+
+
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
